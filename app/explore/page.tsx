10e1291667f8304901ab4a08a636ec5224162db7
ZxStim <thang.victor.luong@gmail.com>
2025-04-02 16:46:02 +0700

    feat: added new section

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
 export default function Explore() {
   return (
-    <main className="min-h-screen bg-foreground pb-8">
-      <h1>Explore</h1>
+    <main className="min-h-screen bg-background pb-8 p-2 md:p-8">
+      <div className="flex flex-col gap-8">
+        <Button asChild variant="outline" className="rounded-full w-fit">
+          <Link href="/">
+            <ArrowLeft />
+            Back
+          </Link>
+        </Button>
+        <h1 className="text-4xl font-bold">Explore</h1>
+      </div>
     </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,25 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 min-h-screen bg-foreground pb-8">
+    <main className="flex flex-col gap-16 min-h-screen bg-background px-2 md:px-8 pb-8">
       {/* Hero Section with just the image and text */}
       <div className="h-screen relative flex flex-col items-center justify-center overflow-hidden">
         <div className="absolute inset-0 z-0 flex items-center justify-center">
-          <div className="relative w-full h-full">
+          <div className="relative">
             <Image
               src="/hero.svg"
               alt="Cosmic Singularity"
-              width={1920}
-              height={1080}
+              width={600}
+              height={600}
               priority
               className="w-screen h-screen object-cover"
             />
-            <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-foreground to-transparent" />
+            <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent" />
           </div>
         </div>
         <div className="relative z-10 text-center px-4">
-          <h1 className="text-4xl md:text-4xl lg:text-6xl font-bold leading-tight text-primary">DeFi Singularity</h1>
-          <Button asChild size="lg" className="p-4 rounded-full mt-4">
+          <h1 className="text-4xl md:text-4xl lg:text-6xl font-bold leading-tight text-secondary">DeFi Singularity</h1>
+          <Button variant="secondary" asChild size="lg" className="p-4 rounded-full mt-4">
             <Link href="/explore">
               <ArrowRight />
               Explore
@@ -32,7 +32,7 @@ export default function Home() {
         </div>
       </div>
       <div className="flex flex-col gap-4 items-center justify-center">
-        <h1 className="text-4xl font-bold text-secondary max-w-sm text-center">Enter a new era of crosschain DeFi</h1>
+        <h1 className="text-4xl font-bold text-primary max-w-sm text-center">Enter a new era of crosschain DeFi</h1>
         <p className="max-w-xs text-lg text-center text-muted-foreground">Trustless and Verifiable interoperability powered by Polkadot</p>
         <Image
           src="/hyperbridge.png"
@@ -42,12 +42,38 @@ export default function Home() {
         />
       </div>
       <div className="flex flex-col gap-4 items-center justify-center">
-        <h1 className="text-4xl font-bold text-secondary max-w-sm text-center">765,000 DOT incentive</h1>
+        <h1 className="text-4xl font-bold text-primary max-w-sm text-center">765,000 DOT incentive</h1>
         <p className="max-w-xs text-lg text-center text-muted-foreground">Paid out in vDOT, to liquidity providers on eligible pools</p>
+        <Image
+          src="/dot.svg"
+          alt="DOT"
+          width={600}
+          height={600}
+        />
       </div>
-      <div className="flex flex-col gap-4 items-center justify-center">
-        <h1 className="text-4xl font-bold text-secondary max-w-sm text-center">Expansive coverage</h1>
-        <p className="max-w-xs text-lg text-center text-muted-foreground">4 ecosystems, 5 protocols</p>
+      <div className="flex flex-col gap-4">
+        <p className="text-muted-foreground">On the event horizon...</p>
+        <h1 className="text-4xl font-bold text-primary">Singularity</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <div className="flex flex-col gap-2 border border-muted rounded-lg p-4">
+            <h2 className="text-lg font-bold">Unified</h2>
+            <p className="text-muted-foreground">5 ecosystems, 6 protocols</p>
+          </div>
+          <div className="flex flex-col gap-2 border border-muted rounded-lg p-4">
+            <h2 className="text-lg font-bold">Liquidity mining</h2>
+            <p className="text-muted-foreground">Provide liquidity and earn rewards</p>
+          </div>
+          <div className="grid grid-cols-1 gap-4">
+            <div className="flex flex-col gap-2 border border-muted rounded-lg p-4">
+              <h2 className="text-lg font-bold">Yield bearing rewards</h2>
+              <p className="text-muted-foreground">Rewards are boosted by being an LST</p>
+            </div>
+            <div className="flex flex-col gap-2 border border-muted rounded-lg p-4">
+              <h2 className="text-lg font-bold">Verifiable security</h2>
+              <p className="text-muted-foreground">Premium blue chip asset backed by Polkadot</p>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
