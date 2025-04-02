11d0f4a0fbef21e9e0f8b361cd46d8cb433b3bfd
ZxStim <thang.victor.luong@gmail.com>
2025-04-02 17:25:18 +0700

    feat: added new copywriting

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,27 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-16 min-h-screen bg-background px-2 md:px-8 pb-8">
+    <main className="flex flex-col gap-24 min-h-screen bg-background pb-8">
       {/* Hero Section with just the image and text */}
       <div className="h-screen relative flex flex-col items-center justify-center overflow-hidden">
         <div className="absolute inset-0 z-0 flex items-center justify-center">
-          <div className="relative">
+          <div className="relative w-full h-full">
             <Image
               src="/hero.svg"
               alt="Cosmic Singularity"
-              width={600}
-              height={600}
+              width={1920}
+              height={1080}
               priority
               className="w-screen h-screen object-cover"
             />
-            <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent" />
           </div>
         </div>
         <div className="relative z-10 text-center px-4">
-          <h1 className="text-4xl md:text-4xl lg:text-6xl font-bold leading-tight text-secondary">DeFi Singularity</h1>
+          <h1 className="text-2xl md:text-3xl lg:text-5xl font-bold leading-tight text-secondary">DeFi Singularity</h1>
           <Button variant="secondary" asChild size="lg" className="p-4 rounded-full mt-4">
             <Link href="/explore">
               <ArrowRight />
@@ -30,8 +29,9 @@ export default function Home() {
             </Link>
           </Button>
         </div>
+        <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background via-background/50 to-transparent" />
       </div>
-      <div className="flex flex-col gap-4 items-center justify-center">
+      <div className="flex flex-col gap-4 items-center justify-center px-2 md:px-8">
         <h1 className="text-4xl font-bold text-primary max-w-sm text-center">Enter a new era of crosschain DeFi</h1>
         <p className="max-w-xs text-lg text-center text-muted-foreground">Trustless and Verifiable interoperability powered by Polkadot</p>
         <Image
@@ -41,7 +41,7 @@ export default function Home() {
           height={700}
         />
       </div>
-      <div className="flex flex-col gap-4 items-center justify-center">
+      <div className="flex flex-col gap-4 items-center justify-cente px-2 md:px-8">
         <h1 className="text-4xl font-bold text-primary max-w-sm text-center">765,000 DOT incentive</h1>
         <p className="max-w-xs text-lg text-center text-muted-foreground">Paid out in vDOT, to liquidity providers on eligible pools</p>
         <Image
@@ -51,7 +51,7 @@ export default function Home() {
           height={600}
         />
       </div>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 px-4 md:px-8">
         <p className="text-muted-foreground">On the event horizon...</p>
         <h1 className="text-4xl font-bold text-primary">Singularity</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
@@ -70,10 +70,13 @@ export default function Home() {
             </div>
             <div className="flex flex-col gap-2 border border-muted rounded-lg p-4">
               <h2 className="text-lg font-bold">Verifiable security</h2>
-              <p className="text-muted-foreground">Premium blue chip asset backed by Polkadot</p>
+              <p className="text-muted-foreground">Assets are bridged by Hyperbridge</p>
             </div>
           </div>
         </div>
+      </div>
+      <div className="flex flex-col gap-4 items-center justify-cente px-2 md:px-8r">
+        <h1 className="text-4xl font-bold text-primary max-w-sm text-center">Powered by Polkadot</h1>
       </div>
     </main>
   );
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer>
+    <footer className="flex flex-col gap-4 px-4 md:px-8 h-20">
       <p>Footer</p>
     </footer>
   );
