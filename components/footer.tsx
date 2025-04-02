6ea404605f5c641982733a3cf49c644cec51da30
ZxStim <thang.victor.luong@gmail.com>
2025-04-02 17:54:55 +0700

    feat: added footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="flex flex-col gap-4 px-4 md:px-8 h-20">
-      <p>Footer</p>
+    <footer className="grid grid-cols-1 gap-4 md:grid-cols-2 items-start px-24 pb-12">
+      <div className="flex flex-row gap-2 items-center">
+        <Image
+          src="/logo.svg"
+          alt="DeFi singularity logo"
+          width={100}
+          height={100}
+        />
+        <p className="text-2xl font-bold">DeFi Singularity</p>
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="flex flex-col gap-2">
+          <h2 className="text-lg font-bold">Resources</h2>
+          <Link href="/">Home</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h2 className="text-lg font-bold">Links</h2>
+          <Link href="/">Home</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+      </div>
     </footer>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
-    <header>
-      <h1>DeFi Singularity</h1>
+    <header className="grid grid-cols-1 md:grid-cols-2">
+      <Image
+        src="/logo.svg"
+        alt="DeFi singularity logo"
+        width={100}
+        height={100}
+      />
+      <div>
+        Resources
+      </div>
     </header>
   );
 }
