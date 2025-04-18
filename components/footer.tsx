19a19aa450c780b00db865ed0ff0aa4933d4896e
ZxStim <thang.victor.luong@gmail.com>
2025-04-18 16:30:38 +0700

    fix: fixed footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="grid grid-cols-1 gap-4 md:grid-cols-2 items-start px-24 pb-16 mt-12 h-[250px]">
+    <footer className="grid grid-cols-1 gap-4 md:grid-cols-2 items-start px-8 md:px-24 mt-12 h-[400px]">
       <div className="flex flex-col gap-4">
-        <div className="flex flex-row gap-2 items-center">
+        <div className="flex flex-row gap-2 items-center h-[80px]">
           <Image
             src="/logo.svg"
             alt="DeFi singularity logo"
@@ -15,7 +15,7 @@ export default function Footer() {
           <p className="text-2xl font-bold">DeFi Singularity</p>
         </div>
       </div>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 gap-8">
         <div className="flex flex-col gap-2">
           <h2 className="text-lg font-bold">Resources</h2>
           <Link
