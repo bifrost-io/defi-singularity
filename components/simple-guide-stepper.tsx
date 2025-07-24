c3223d3c6b0050c634109e27bf574e436cb0cf64
ZxStim <zxstim@gmail.com>
2025-07-24 15:36:43 +0700

    feat: mint step

--- components/simple-guide-stepper.tsx
+++ components/simple-guide-stepper.tsx
@@ -54,7 +54,7 @@ export default function SimpleGuideStepper({ poolData }: { poolData: Pool }) {
                 <ExternalLink />
               </Link>
             </Button>
-            {poolData?.baseAsset === "DOT" ? (
+            {/* {poolData?.baseAsset === "DOT" ? (
               <ul className="flex flex-col gap-4 list-disc list-inside text-muted-foreground">
                 <li>
                   Buying from a centralized exchange like Binance, OKX, Bybit or
@@ -129,7 +129,7 @@ export default function SimpleGuideStepper({ poolData }: { poolData: Pool }) {
                   </Link>
                 </li>
               </ul>
-            )}
+            )} */}
           </div>
           <div className="h-[20px]" />
         </div>
@@ -138,6 +138,37 @@ export default function SimpleGuideStepper({ poolData }: { poolData: Pool }) {
         <div className="flex flex-col">
           <div className="rounded-full p-4 flex items-center justify-center h-10 w-10 border border-muted">
             2
+          </div>
+          <div className="border-l-2 border-muted h-full w-0 ml-5" />
+        </div>
+        <div className="flex flex-col w-full">
+          <div className="flex flex-col p-4 gap-4 border border-muted rounded-lg transition-all duration-200 hover:-translate-y-1 hover:shadow-lg hover:shadow-foreground/10">
+            <h1 className="text-xl">Mint {poolData?.baseAsset} on Bifrost</h1>
+            <p className="text-lg text-muted-foreground">
+              You can bridge between different chains by using{" "}
+              <Image
+                src="/hyperbridge.svg"
+                alt="Hyperbridge"
+                width={20}
+                height={20}
+                className="inline mb-2"
+              />{" "}
+              Hyperbridge
+            </p>
+            <Button asChild className="w-fit self-end">
+              <Link target="_blank" href="https://app.hyperbridge.network/">
+                Go to Hyperbridge
+                <ExternalLink />
+              </Link>
+            </Button>
+          </div>
+          <div className="h-[20px]" />
+        </div>
+      </div>
+      <div className="flex flex-row gap-4">
+        <div className="flex flex-col">
+          <div className="rounded-full p-4 flex items-center justify-center h-10 w-10 border border-muted">
+            3
           </div>
           <div className="border-l-2 border-muted h-full w-0 ml-5" />
         </div>
@@ -168,7 +199,7 @@ export default function SimpleGuideStepper({ poolData }: { poolData: Pool }) {
       <div className="flex flex-row gap-4">
         <div className="flex flex-col">
           <div className="rounded-full p-4 flex items-center justify-center h-10 w-10 border border-muted">
-            3
+            4
           </div>
         </div>
         <div className="flex flex-col w-full">
