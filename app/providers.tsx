d5eb87ef97e642f88598ada04af225e70d06a107
ZxStim <zxstim@gmail.com>
2025-07-25 17:09:48 +0700

    feat: updated apr

---
+++ app/providers.tsx
@@ -0,0 +1,16 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    // Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
