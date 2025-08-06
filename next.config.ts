0e0bb6f30ad17cb0f74887fd14e4f0d6afb8fc6b
ZxStim <zxstim@gmail.com>
2025-08-06 14:32:55 +0700

    fix: nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  webpack: config => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
 };
 
 export default nextConfig;
