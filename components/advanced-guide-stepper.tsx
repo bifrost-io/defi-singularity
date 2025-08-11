d5236aa0fa7fa86c5918b586d430e133222558e4
ZxStim <zxstim@gmail.com>
2025-08-11 13:08:04 +0700

    feat: revised advanced guide

--- components/advanced-guide-stepper.tsx
+++ components/advanced-guide-stepper.tsx
@@ -1,10 +1,23 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ExternalLink } from "lucide-react";
 import { Pool } from "@/app/explore/data";
 import Image from "next/image";
+import dynamic from "next/dynamic";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useAccount } from 'wagmi';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+const ZapComponent = dynamic(() => import("@/components/zap-component"), {
+  ssr: false,
+  loading: () => <Skeleton className="w-full h-[500px]" />,
+});
 
 export default function AdvancedGuideStepper({ poolData }: { poolData: Pool }) {
+  const { address } = useAccount();
+  
   return (
     <div className="flex flex-col w-full md:w-3/4">
       <div className="flex flex-row gap-4">
@@ -212,15 +225,13 @@ export default function AdvancedGuideStepper({ poolData }: { poolData: Pool }) {
       <div className="flex flex-row gap-4">
         <div className="flex flex-col">
           <div className="rounded-full p-4 flex items-center justify-center h-10 w-10 border border-muted">
-            4
+            1
           </div>
           <div className="border-l-2 border-muted h-full w-0 ml-5" />
         </div>
         <div className="flex flex-col w-full">
           <div className="flex flex-col p-4 gap-4 border border-muted rounded-lg transition-all duration-200 hover:-translate-y-1 hover:shadow-lg hover:shadow-foreground/10">
-            <h1 className="text-xl">
-              Add {poolData?.baseAsset} / {poolData?.quoteAsset} liquidity
-            </h1>
+            <h1 className="text-xl">Add {poolData?.baseAsset} / {poolData?.quoteAsset} liquidity</h1>
             <p className="text-lg text-muted-foreground">
               On the{" "}
               <Image
@@ -237,36 +248,35 @@ export default function AdvancedGuideStepper({ poolData }: { poolData: Pool }) {
                 width={20}
                 height={20}
                 className="inline mb-2"
-              />{" "}
-              {poolData?.protocol} V4{" "}
+              />{" "}{poolData?.protocol} V4 {" "}
               <Image
                 src={poolData?.baseAssetLogo || ""}
                 alt={poolData?.baseAsset || ""}
                 width={20}
                 height={20}
                 className="inline mb-2"
-              />{" "}
-              {poolData?.baseAsset} /{" "}
+              />{" "}{poolData?.baseAsset}{" "}
+              / {" "}
               <Image
                 src={poolData?.quoteAssetLogo || ""}
                 alt={poolData?.quoteAsset || ""}
                 width={20}
                 height={20}
                 className="inline mb-2"
-              />{" "}
-              {poolData?.quoteAsset} 0.3% pool. You can choose an appropriate
-              liquidity range and token amounts based on your risk tolerance and
-              preferences. Please note that only active liquidity within the
-              current price range is eligible to participate in farming.
-              Unilateral liquidity that is temporarily inactive will likely not
-              receive any incentives during the event period.
+              />{" "}{poolData?.quoteAsset}{" "}
+              0.3% pool by using the Zap in button below or manually via Uniswap interface. You can choose an appropriate liquidity range and token amounts based on your risk tolerance and preferences. Please note that only active liquidity within the current price range is eligible to participate in farming. Unilateral liquidity that is temporarily inactive will likely not receive any incentives during the event period.
             </p>
-            <Button asChild className="w-fit self-end">
-              <Link target="_blank" href={poolData?.poolUrl || ""}>
-                Add liquidity
-                <ExternalLink />
-              </Link>
-            </Button>
+            <div className="flex flex-row gap-2 self-end">
+              <Button variant="outline" asChild className="w-fit self-end">
+                <Link target="_blank" href={poolData?.poolUrl || ""}>
+                  Add manually
+                  <ExternalLink />
+                </Link>
+              </Button>
+              {
+                address ? <ZapComponent pageChainId={poolData?.chainId} /> : <ConnectButton />
+              }
+            </div>
           </div>
           <div className="h-[20px]" />
         </div>
