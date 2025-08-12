f4cf921f625fc69389075b60b6b168b2654b7ea8
ZxStim <zxstim@gmail.com>
2025-08-12 18:28:49 +0700

    feat: improved zap widget

--- components/zap-component.tsx
+++ components/zap-component.tsx
@@ -9,7 +9,7 @@ import { useConnectModal } from "@rainbow-me/rainbowkit";
 import { useSendTransaction } from "wagmi";
 // import { useMediaQuery } from "@/hooks/use-media-query";
 // import { Button } from "@/components/ui/button";
-// import { Zap } from "lucide-react";
+import { Zap } from "lucide-react";
 // import {
 //   Dialog,
 //   DialogContent,
@@ -31,61 +31,67 @@ export default function ZapComponent({ pageChainId }: { pageChainId: number }) {
   const { sendTransactionAsync } = useSendTransaction();
 
   return (
-    <LiquidityWidget
-      theme={{
-        text: "#ffffff",
-        subText: "#979797",
-        icons: "#a9a9a9",
-        layer1: "#1c1c1c",
-        dialog: "#1c1c1c",
-        layer2: "#313131",
-        stroke: "#313131",
-        chartRange: "#28e0b9",
-        chartArea: "#047855",
-        accent: "#31cb9e",
-        warning: "#ff9901",
-        error: "#ff537b",
-        success: "#189470",
-        blue: "#2c9ce4",
-        fontFamily: "Inter",
-        borderRadius: "20px",
-        buttonRadius: "24px",
-        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.04)",
-      }}
-      chainId={pageChainId}
-      poolType={PoolType.DEX_UNISWAP_V4}
-      poolAddress="0xaa73a142ee6a70b2f2e3311c9dee917f1210be2abbc4385467935ceaaadab8a0"
-      connectedAccount={{ address: address, chainId: chainId }}
-      onConnectWallet={() => {
-        openConnectModal?.();
-      }}
-      onSwitchChain={() => switchChain({ chainId: pageChainId })}
-      onSubmitTx={async (txData: {
-        from: string;
-        to: string;
-        value: string;
-        data: string;
-        gasLimit: string;
-      }): Promise<string> => {
-        if (!address) {
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold flex items-center gap-2"><Zap /> Zap</h1>
+      <p className="text-sm text-muted-foreground">
+        Zap in is a simple way to swap and add liquidity to the pool.
+      </p>
+      <LiquidityWidget
+        theme={{
+          text: "#ffffff",
+          subText: "#979797",
+          icons: "#a9a9a9",
+          layer1: "#1c1c1c",
+          dialog: "#1c1c1c",
+          layer2: "#313131",
+          stroke: "#313131",
+          chartRange: "#28e0b9",
+          chartArea: "#047855",
+          accent: "#31cb9e",
+          warning: "#ff9901",
+          error: "#ff537b",
+          success: "#189470",
+          blue: "#2c9ce4",
+          fontFamily: "Inter",
+          borderRadius: "20px",
+          buttonRadius: "24px",
+          boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.04)",
+        }}
+        chainId={pageChainId}
+        poolType={PoolType.DEX_UNISWAP_V4}
+        poolAddress="0xaa73a142ee6a70b2f2e3311c9dee917f1210be2abbc4385467935ceaaadab8a0"
+        connectedAccount={{ address: address, chainId: chainId }}
+        onConnectWallet={() => {
           openConnectModal?.();
-          throw new Error("No wallet connected");
-        }
-        try {
-          const hash = await sendTransactionAsync({
-            account: txData.from as `0x${string}`,
-            to: txData.to as `0x${string}`,
-            data: txData.data as `0x${string}`,
-            value: BigInt(txData.value),
-          });
-          return hash;
-        } catch (e) {
-          console.log(e);
-          throw e;
-        }
-      }}
-      source="bifrost"
-    />
+        }}
+        onSwitchChain={() => switchChain({ chainId: pageChainId })}
+        onSubmitTx={async (txData: {
+          from: string;
+          to: string;
+          value: string;
+          data: string;
+          gasLimit: string;
+        }): Promise<string> => {
+          if (!address) {
+            openConnectModal?.();
+            throw new Error("No wallet connected");
+          }
+          try {
+            const hash = await sendTransactionAsync({
+              account: txData.from as `0x${string}`,
+              to: txData.to as `0x${string}`,
+              data: txData.data as `0x${string}`,
+              value: BigInt(txData.value),
+            });
+            return hash;
+          } catch (e) {
+            console.log(e);
+            throw e;
+          }
+        }}
+        source="bifrost"
+      />
+    </div>
   );
 
   // return (
